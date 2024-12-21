d2651e1a9cb762eed5bbb65f5acb3336a34df1af
khanshahnwaz <shahnwazkhan2007@gmail.com>
2024-12-21 13:37:37 +0530

    Transformed in serverless app, using api directory

---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  app.get('/',(req, res) => {
+    res.send('Hello! Welcome to DSA_Tracker.');
+  })
+const dsaRoute = require('./routes/dsa');
+app.use('/api/dsa', dsaRoute);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
